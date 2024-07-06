03e797c9d92031f149d08af4d8d88c51fac309bf
govindKulk <kulkarnigovind2003@gmail.com>
2024-07-07 02:15:41 +0530

    corrected flow bg

--- src/components/herosection/FlowSection.jsx
+++ src/components/herosection/FlowSection.jsx
@@ -23,7 +23,7 @@ const FlowSection = () => {
  gap-4 p-4 sm:p-0 relative
     '>
         <h3 className='text-xl text-center py-4'>How Do I  <span className="text-[#1a73e8] font-bold">Refer?</span></h3>
-        <img src='/public/flowbg.png' alt='flowbg' className='max-w-screen-lg mx-auto w-full 
+        <img src='/flowbg.png' alt='flowbg' className='max-w-screen-lg mx-auto w-full 
         absolute block left-1/2 -translate-x-1/2 z-10 min-w-[1000px] min-h-[350px]' />
         <div className="flex  max-w-screen-lg mx-auto w-full h-full justify-evenly items-center">
         {
