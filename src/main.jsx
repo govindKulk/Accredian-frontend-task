c08e88d358d0a7863a62f953a1e92fdfca5d5f27
govindKulk <kulkarnigovind2003@gmail.com>
2024-07-16 00:50:36 +0530

    changed import for context.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from './ErrorPage.jsx'
 import Dashboard from './routes/Dashboard.jsx'
 import Root from './routes/Root.jsx'
-import { AuthProvider } from './context.jsx'
+import { AuthProvider } from './context'
 import Profile from './routes/Profile.jsx'
 
 const router = createBrowserRouter([
